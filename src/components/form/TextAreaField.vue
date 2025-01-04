dedafc629f4545ad4f178dc00f206dcc1c50c78b
faez <benfaez07@gmail.com>
2025-01-04 05:08:11 +0100

    feat(form): add reusable TextAreaField component
    - Created a reusable TextAreaField component for managing textarea inputs.
    - Includes support for labels, placeholder, error messages, and rows.
    - Styled with Tailwind classes to ensure consistency across the application.
    - Ensures v-model compatibility and validation error display.

---
+++ src/components/form/TextAreaField.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="w-full mt-3">
+      <label
+        v-if="label"
+        :for="name"
+        class="block text-sm font-medium text-primary"
+      >
+        {{ label }}
+      </label>
+      <textarea
+        :id="name"
+        v-model="model"
+        :placeholder="placeholder"
+        :rows="rows"
+        class="block w-full rounded-md border mt-1 px-3 py-2 border-tertiary shadow-sm focus:outline-quaternary sm:text-sm"
+        :class="{
+          'border-red-500 focus:ring-red-500 focus:border-red-500': error,
+        }"
+      ></textarea>
+      <p v-if="error" class="mt-2 text-sm text-red-600">{{ error }}</p>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { defineProps } from "vue";
+  
+  const model = defineModel<string>();
+  
+  defineProps({
+    modelValue: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: false,
+    },
+    placeholder: {
+      type: String,
+      required: false,
+    },
+    rows: {
+      type: Number,
+      default: 3,
+    },
+    error: {
+      type: String,
+      required: false,
+    },
+  });
+  </script>
+  
